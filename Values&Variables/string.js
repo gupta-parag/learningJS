10a03a5cf774d2cb4192be8df3e450b614e27b7c
gupta-parag <parag.gupta@protonmail.com>
2023-11-02 21:17:47 -0400

    Added string methods

--- Values&Variables/string.js
+++ Values&Variables/string.js
@@ -15,7 +15,19 @@ console.log(z.trim().toUpperCase());
 console.log("Z length is : " + z.trim().toUpperCase().length);
 
 // String methods with arguments
+
+// indexOf()
 console.log("Index of letter O : ", "abcdefgho".indexOf("o"));
 console.log("Index of word dog : ", "abcdogho".indexOf("dog"));
 console.log("Index of word ball : ", "aballcdlaballogho".indexOf("ball")); // only tells you the first occurence of the pattern
 console.log("Index of letter O : ", "abcdefgh".indexOf("o")); // if -1 that means it is not found
+
+//slice()
+console.log("SLICE");
+console.log("abcdefgho".slice(5)); // from the starting index till the end
+console.log("abcdefgho".slice(5, 7)); // from starting till the end
+console.log("Hoa many letters from 15 th position : ", "abcdefgho".slice(15)); // from starting till the end
+
+//replace()
+
+console.log("baseball is entertaining".replace("baseball", "home before dark"));
