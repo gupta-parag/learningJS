595643eb88fa7312c364efb2ea2ecc1cf6612509
gupta-parag <parag.gupta@protonmail.com>
2023-11-02 20:44:45 -0400

    Added string methods

--- Values&Variables/string.js
+++ Values&Variables/string.js
@@ -15,3 +15,7 @@ console.log(z.trim().toUpperCase());
 console.log("Z length is : " + z.trim().toUpperCase().length);
 
 // String methods with arguments
+console.log("Index of letter O : ", "abcdefgho".indexOf("o"));
+console.log("Index of word dog : ", "abcdogho".indexOf("dog"));
+console.log("Index of word ball : ", "aballcdlaballogho".indexOf("ball")); // only tells you the first occurence of the pattern
+console.log("Index of letter O : ", "abcdefgh".indexOf("o")); // if -1 that means it is not found
