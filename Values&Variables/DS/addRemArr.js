1c427aa92f1199ec06082bff60d07f37c8c31cb2
gupta-parag <parag.gupta@protonmail.com>
2023-11-04 15:41:10 -0400

    Added shopping list

---
+++ Values&Variables/DS/addRemArr.js
@@ -0,0 +1,22 @@
+let shoppingList = ["eggs", "cereal", "cheese"];
+
+// Array methods to add and\or remove elements
+console.log(shoppingList.push("milk"));
+console.log(shoppingList);
+
+//Deleting element from the back of the array
+console.log(shoppingList.pop());
+console.log(shoppingList);
+
+// Deleting fom the first posiiton of the array
+console.log(shoppingList.shift());
+console.log(shoppingList);
+
+//Entering elements on the first position of the array
+console.log(shoppingList.unshift("lemon bread"));
+console.log(shoppingList);
+console.log(shoppingList.unshift("pineapple"));
+console.log(shoppingList.unshift("papaya"));
+console.log(shoppingList);
+console.log(shoppingList.shift());
+console.log(shoppingList);
---
+++ Values&Variables/DS/arrays.js
@@ -0,0 +1,12 @@
+let numbers = [12, 56, 34, 86, 4, 33];
+let shoppingList = ["eggs", "cereal", "cheese"];
+
+shoppingList[2] = "milk";
+shoppingList[shoppingList.length] = "tofu";
+shoppingList[shoppingList.length] = "cottage cheese";
+
+console.log(numbers);
+console.log(shoppingList);
+
+let myCollecton = [12, "cat", "45", 23, "chill"];
+console.log(myCollecton);
